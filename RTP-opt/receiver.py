86dc4a70df65ffe7e65274429fed4b2898a7da3c
Kuro-orzz <23020674@vnu.edu.vn>
2025-04-25 15:19:07 -0400

    selective repeat

--- RTP-opt/receiver.py
+++ RTP-opt/receiver.py
@@ -1,45 +1,98 @@
 import argparse
 import socket
+import sys
+import time
 
 from utils import PacketHeader, compute_checksum
 
+ip = "0.0.0.0"
+port = 12345
+sz = 128
+
+START = 0
+END = 1
+DATA = 2
+ACK = 3
+
+# send ACK packet
+def send_ACK_packet(sock, address, seqNum):
+	pkt_header = PacketHeader(type=3, seq_num=seqNum, length=0)
+	sock.sendto(bytes(pkt_header), address)
 
 def receiver(receiver_ip, receiver_port, window_size):
-    """TODO: Listen on socket and print received message to sys.stdout."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((receiver_ip, receiver_port))
-    while True:
-        # Receive packet; address includes both IP and port
-        pkt, address = s.recvfrom(2048)
+	"""TODO: Listen on socket and print received message to sys.stdout."""
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((receiver_ip, receiver_port))
 
-        # Extract header and payload
-        pkt_header = PacketHeader(pkt[:16])
-        msg = pkt[16 : 16 + pkt_header.length]
+	expected_seqNum = 0
+	buffer = dict()
 
-        # Verity checksum
-        pkt_checksum = pkt_header.checksum
-        pkt_header.checksum = 0
-        computed_checksum = compute_checksum(pkt_header / msg)
-        if pkt_checksum != computed_checksum:
-            print("checksums not match")
-        print(msg)
+	while True:
+		# Receive packet; address includes both IP and port
+		pkt, address = s.recvfrom(1472)
 
+		# Extract header and payload
+		pkt_header = PacketHeader(pkt[:16])
+		msg = pkt[16 : 16 + pkt_header.length]
+		recv_checksum = pkt_header.checksum
+
+		# checksum to detect if bit error or not
+		pkt_header.checksum = 0
+		new_checksum = compute_checksum(pkt_header / msg)
+		if recv_checksum != new_checksum:
+			continue
+
+		# check if it out of range of sliding window
+		if expected_seqNum + window_size <= pkt_header.seq_num:
+			continue
+
+		# if packet not equal expected packet, send ACK
+		if pkt_header.seq_num != expected_seqNum:
+			buffer[pkt_header.seq_num] = msg
+			send_ACK_packet(s, address, pkt_header.seq_num)
+			continue
+
+		# Handle START, END packet
+		if pkt_header.type == START:
+			expected_seqNum += 1
+			send_ACK_packet(s, address, expected_seqNum)
+			continue
+		if pkt_header.type == END:
+			expected_seqNum += 1
+			send_ACK_packet(s, address, expected_seqNum)
+			break
+
+		# recv valid and inorder packet, send back ACK
+		expected_seqNum += 1
+		sys.stdout.buffer.write(msg)
+		sys.stdout.buffer.flush()
+		while(expected_seqNum in buffer):
+			chunk = buffer.pop(expected_seqNum)
+			sys.stdout.buffer.write(chunk)
+			sys.stdout.buffer.flush()
+			expected_seqNum += 1
+
+		send_ACK_packet(s, address, expected_seqNum)
+
+	s.close()
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "receiver_ip", help="The IP address of the host that receiver is running on"
-    )
-    parser.add_argument(
-        "receiver_port", type=int, help="The port number on which receiver is listening"
-    )
-    parser.add_argument(
-        "window_size", type=int, help="Maximum number of outstanding packets"
-    )
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	if len(sys.argv) == 4:
+		parser.add_argument(
+			"receiver_ip", help="The IP address of the host that receiver is running on"
+		)
+		parser.add_argument(
+			"receiver_port", type=int, help="The port number on which receiver is listening"
+		)
+		parser.add_argument(
+			"window_size", type=int, help="Maximum number of outstanding packets"
+		)
+		args = parser.parse_args()
 
-    receiver(args.receiver_ip, args.receiver_port, args.window_size)
-
+		receiver(args.receiver_ip, args.receiver_port, args.window_size)
+	else:
+		receiver(ip, port, sz)
 
 if __name__ == "__main__":
-    main()
+	main()
--- RTP-opt/sender.py
+++ RTP-opt/sender.py
@@ -1,32 +1,122 @@
 import argparse
 import socket
+import sys
 
+from time import sleep
 from utils import PacketHeader, compute_checksum
 
+ip = "0.0.0.0"
+port = 12345
+sz = 128
+
+START = 0
+END = 1
+DATA = 2
+ACK = 3
+MAX_PAYLOAD = 1456
+
+# Wait for response
+def wait_for_ack(sock, seqNum) -> int:
+    nextSeq = seqNum
+    ack_list = []
+    try:
+        while True:
+            data, _ = sock.recvfrom(1472)
+            ack = PacketHeader(data[:16])
+            if ack.type == 3 and ack.seq_num > nextSeq:
+                nextSeq = ack.seq_num
+            if ack.type == 3:
+                print(ack.seq_num)
+                ack_list.append(ack.seq_num)
+    except socket.timeout:
+        return nextSeq, ack_list
+
+def wait_end_ack(sock, seqNum) -> bool:
+    try:
+        data, _ = sock.recvfrom(1472)
+        ack = PacketHeader(data[:16])
+        if ack.type == 3 and ack.seq_num == seqNum + 1:
+            return True
+    except socket.timeout:
+        return False
+
+# Send start packet and wait for ACK, if not receive, resend packet
+# Timeout for START and DATA is 0.1s, END is 0.5s
+def send_packet(sock, recv_ip, recv_port, pkt_type, seqNum) -> bool:
+    pkt_header = PacketHeader(type=pkt_type, seq_num=seqNum, length=0)
+    pkt_header.checksum = compute_checksum(pkt_header)
+    sock.sendto(bytes(pkt_header), (recv_ip, recv_port))
+    if pkt_type != END:
+        num, ack_list = wait_for_ack(sock, seqNum)
+        if num == seqNum + 1:
+            return True, ack_list
+    if pkt_type == END and wait_end_ack(sock, seqNum):
+        return True, []
+    return False, []
+
+def split_message(message, chunk_size):
+    chunks = [message[i:i + chunk_size] for i in range(0, len(message), chunk_size)]
+    return chunks
 
 def sender(receiver_ip, receiver_port, window_size):
     """TODO: Open socket and send message from sys.stdin."""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    pkt_header = PacketHeader(type=2, seq_num=10, length=14)
-    pkt_header.checksum = compute_checksum(pkt_header / "Hello, world!\n")
-    pkt = pkt_header / "Hello, world!\n"
-    s.sendto(bytes(pkt), (receiver_ip, receiver_port))
+    s.settimeout(0.5)
+    msg = sys.stdin.buffer.read()
 
+    seqNum = 0
+    buffer = set()
+
+    # handle START packet
+    start_ack = False
+    while not start_ack:
+        check, _ = send_packet(s, receiver_ip, receiver_port, START, seqNum)
+        if check:
+            seqNum += 1
+            start_ack = True
+
+    # Split message and make packet
+    listMsg = split_message(msg, MAX_PAYLOAD)
+    msgPacket = [0]
+    for i in range(len(listMsg)):
+        pkt_header = PacketHeader(type=2, seq_num=i+1, length=len(listMsg[i]))
+        pkt_header.checksum = compute_checksum(pkt_header / listMsg[i])
+        pkt = pkt_header / listMsg[i]
+        msgPacket.append(pkt)
+
+    # Send DATA packet
+    while seqNum <= len(listMsg):
+        limit = min(seqNum+window_size+1, len(listMsg)+1)
+        for i in range(seqNum, limit):
+            if i in buffer:
+                continue
+            s.sendto(bytes(msgPacket[i]), (receiver_ip, receiver_port))
+        num, ack_list= wait_for_ack(s, seqNum)
+        seqNum = max(seqNum, num)
+        for i in ack_list:
+            buffer.add(i)
+
+    # handle END packet
+    end_ack = send_packet(s, receiver_ip, receiver_port, END, seqNum)
+    s.close()
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "receiver_ip", help="The IP address of the host that receiver is running on"
-    )
-    parser.add_argument(
-        "receiver_port", type=int, help="The port number on which receiver is listening"
-    )
-    parser.add_argument(
-        "window_size", type=int, help="Maximum number of outstanding packets"
-    )
-    args = parser.parse_args()
+    if len(sys.argv) == 4:
+        parser.add_argument(
+            "receiver_ip", help="The IP address of the host that receiver is running on"
+        )
+        parser.add_argument(
+            "receiver_port", type=int, help="The port number on which receiver is listening"
+        )
+        parser.add_argument(
+            "window_size", type=int, help="Maximum number of outstanding packets"
+        )
+        args = parser.parse_args()
 
-    sender(args.receiver_ip, args.receiver_port, args.window_size)
+        sender(args.receiver_ip, args.receiver_port, args.window_size)
+    else:
+        sender(ip, port, sz)
 
 
 if __name__ == "__main__":
